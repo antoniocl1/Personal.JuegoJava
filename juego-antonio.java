3666d166e8cd6876d75642021f0e890970fc679d
Antonio <antoniojesuscorteslopez433@gmail.com>
2025-06-03 12:24:51 +0200

    instancias y bienvenida al juego

--- juego-antonio.java
+++ juego-antonio.java
@@ -9,3 +9,21 @@ public class Juego {
     // Instancias de las clases Jugador y Pinguino
     static Pinguino pin;
     static Jugador explorador;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Inicialización de objetos
+        explorador = new Jugador();
+        pin = new Pinguino();
+
+        // Mensaje de bienvenida
+        System.out.println("**********************************************************");
+        System.out.println("*                                                        *");
+        System.out.println("*  ❄️ ¡BIENVENIDO A LA ISLA DEL PINGÜINO MUTANTE! ❄️    *");
+        System.out.println("*                                                        *");
+        System.out.println("*   ¿Estás preparado para enfrentarte a criaturas        *");
+        System.out.println("*   heladas, con aletas y MUY MALAS PULGAS?              *");
+        System.out.println("*                                                        *");
+        System.out.println("*   Lucha, corre o... lanza pescado. Sí, como lo oyes.   *");
+        System.out.println("*                                                        *");
+        System.out.println("**********************************************************");
