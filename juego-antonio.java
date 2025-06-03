41a20f6391eb73e79dbb7dfd40b9fa05873a3b4a
Antonio <antoniojesuscorteslopez433@gmail.com>
2025-06-03 12:19:42 +0200

    Inicio de juego

---
+++ juego-antonio.java
@@ -0,0 +1,11 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Juego {
+
+    // Nombre del jugador, inicializado por defecto
+    static String nombre = "Explorador Sin Nombre";
+
+    // Instancias de las clases Jugador y Pinguino
+    static Pinguino pin;
+    static Jugador explorador;
