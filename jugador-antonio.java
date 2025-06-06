a866f24411c77bd4a781df192f08e2fb6cb095b4
Antonio <antoniojesuscorteslopez433@gmail.com>
2025-06-06 09:22:03 +0200

    100

--- jugador-antonio.java
+++ jugador-antonio.java
@@ -4,3 +4,21 @@ public class Jugador {
     int puntos_de_vida = 50;
     String camino = "S";
     int decision = 0;
+
+    /**
+     * Método que evalúa si el jugador puede huir.
+     * Solo se permite huir si el jugador tiene pescados disponibles.
+     *
+     * @param decision Valor 1 (luchar) o 2 (huir).
+     * @param pescado Número de pescados disponibles.
+     * @return true si puede huir o luchar; false si intenta huir sin pescados.
+     */
+    public boolean lucharHuir(int decision, int pescado) {
+        boolean resultado = true;
+        if (pescado > 0) {
+            resultado = true;
+        } else if (pescado == 0 && decision == 2) {
+            return resultado = false;
+        }
+        return resultado;
+    }
