caa94ef73324998b14acdeedebedbea948e0dc10
Antonio <antoniojesuscorteslopez433@gmail.com>
2025-06-06 09:18:33 +0200

    creación del jugador

---
+++ jugador.java
@@ -0,0 +1,6 @@
+public class Jugador {
+    // Atributos del jugador: nombre, puntos de vida, dirección actual y decisión tomada
+    String nombre = " ";
+    int puntos_de_vida = 50;
+    String camino = "S";
+    int decision = 0;
